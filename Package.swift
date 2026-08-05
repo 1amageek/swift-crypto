9073060925f7a9104736f6b72608945a6468cf6d
1amageek <tmy0x3@icloud.com>
2026-08-05 12:21:51 +0900

    Use canonical swift-ssl package declaration

--- Package.swift
+++ Package.swift
@@ -50,7 +50,7 @@ let package = Package(
     ],
     dependencies: [
         // The Pure Swift backend is provided by swift-ssl/SSLCrypto.
-        .package(name: "swift-ssl", url: "https://github.com/1amageek/swift-ssl.git", branch: "main"),
+        .package(url: "https://github.com/1amageek/swift-ssl.git", branch: "main"),
     ],
     targets: [
         .target(
